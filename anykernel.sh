a931d14a0fd3a5aa505feead550c5828b1cb19d5
scafroglia93 <scafroglia57@gmail.com>
2018-01-23 23:04:46 +0100

    bardock: hardcorized
    Signed-off-by: scafroglia93 <scafroglia57@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,20 +4,20 @@
 ## AnyKernel setup
 # begin properties
 properties() {
-kernel.string=
+kernel.string=HardcoreKernel for Aquaris X / X Pro by @scafroglia93
 do.devicecheck=1
 do.modules=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=
-device.name2=
+device.name1=bardock
+device.name2=bardock-pro
 device.name3=
 device.name4=
 device.name5=
 } # end properties
 
 # shell variables
-block=;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
